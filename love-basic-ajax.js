f35d72993bd847a30f9db00ba261a09623af6522
abraham <abe@unscene.us>
2015-09-02 23:52:58 -0700

    -added noConflict() as it solves most of my problems -reordered items to single plugin file

--- love-basic-ajax.js
+++ love-basic-ajax.js
@@ -1,6 +1,7 @@
-jQuery( document ).on( 'click', '.love-button', function() {
-	var post_id = jQuery(this).data('id');
-	jQuery.ajax({
+$fixJq = jQuery.noConflict();
+$fixJq( document ).on( 'click', '.love-button', function() {
+	var post_id = $fixJq(this).data('id');
+	$fixJq.ajax({
 		url : postlove.ajax_url,
 		type : 'post',
 		data : {
@@ -8,7 +9,9 @@ jQuery( document ).on( 'click', '.love-button', function() {
 			post_id : post_id
 		},
 		success : function( response ) {
-			alert(response)
+			$fixJq('#love-count').html( response );
 		}
 	});
+
+	return false;
 })
